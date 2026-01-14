ab7848264fcfcde7ceb6c7ff3ffa7666f6b146be
Kirill Zolotaryov <252222223a@gmail.com>
2026-01-15 00:41:14 +0300

    done

--- script.js
+++ script.js
@@ -6,11 +6,43 @@ function getId(){
             return 1
     }
 
+function getText(){
+    const input = document.getElementById('inputText')
+    return input.value
+}
+
+function deleteDeal(id){
+    const deleteItem = document.getElementById(id)
+    deleteItem.remove()
+}
+
+function deleteInputValue(){
+    const input = document.getElementById('inputText')
+    input.value = ''
+}
+
 function addDeal(deal){
-    const parent = document.getElementById('todoContainer')
-    const child = document.createElement('div')
-    child.classList.add('todo')
-    child.id = getId()
-    child.textContent='fuwebfuew'
-    parent.append(child)
+    if (deal == ""){
+        alert('Введите название напоминания')
+    }
+    else{
+        const parent = document.getElementById('todoContainer')
+        const child = document.createElement('div')
+        const childDeleteButton = document.createElement('button')
+        child.classList.add('todo')
+        child.id = `div-${getId()}`
+        child.textContent = deal
+        parent.append(child)
+        childDeleteButton.classList.add('deleteButton')
+        childDeleteButton.id = `button-${getId()-1}`
+        childDeleteButton.textContent = 'Удалить'
+        childDeleteButton.addEventListener('click', function(){
+            let id = this.id
+            let lastChar = id[id.length-1]
+            id =  `div-${lastChar}`
+            deleteDeal(id)
+        })
+        child.append(childDeleteButton)
+        deleteInputValue()
+    }
 }
